d8302c7679259b097b0fd865940deb75ce4c18e9
liuyuhn <15609845089@163.com>
2020-04-30 16:59:41 +0800

    注册路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,11 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'user-home', component: UserComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
